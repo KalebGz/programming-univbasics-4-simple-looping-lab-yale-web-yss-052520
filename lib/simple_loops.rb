84ac9d9536d2a320c7b34e3d6570b01ba65cf6eb
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 04:35:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,10 +24,10 @@ def output_array(array)
   end
 end
 
-def output_array(array)
+def return_string_array(array)
   count = 0
   while (count < array.length) do
-    puts array[count]
+    array[count]
     count+=1
   end
 end
